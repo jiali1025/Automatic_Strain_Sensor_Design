3ede9e03eb500c967d7c9bd74f34747caffe01f9
Lim Kai Zhuo <limkaizhuo@gmail.com>
2020-11-26 11:08:14 +0800

    Updating Code

--- run_hparam_opt.py
+++ run_hparam_opt.py
@@ -5,9 +5,13 @@ from own_package.features_labels_setup import load_data_to_fl, load_testset_to_f
 def selector(case, **kwargs):
     if case == 1:
         fl = load_data_to_fl(kwargs['loader_excel'], normalise_labels=False, norm_mask=[0, 1, 3, 4, 5])
-        fl_store = fl.create_kf(k_folds=kwargs['k_folds'])
-        #fl_store = fl.fold_invariant_kf_augment(numel=5, k_folds=10)
-        #fl_store = fl.fold_smote_kf_augment(numel=1100, k_folds=10)
+        if kwargs['data_augmentation'] == 'smote':
+            fl_store = fl.fold_smote_kf_augment(numel=kwargs['numel'], k_folds=kwargs['k_folds'])
+        elif kwargs['data_augmentation'] == 'invariant':
+            fl_store = fl.fold_invariant_kf_augment(numel=kwargs['numel'], k_folds=kwargs['k_folds'])
+        else:
+            fl_store = fl.create_kf(k_folds=kwargs['k_folds'])
+
         other_fl_dict = {k: load_testset_to_fl(v, norm_mask=[0, 1, 3, 4, 5], scaler=fl.scaler) for k, v in
                          zip(kwargs['other_names'], kwargs['other_dir'])}
         write_dir = create_results_directory(f'./results/hparam_opt/{kwargs["write_dir_name"]}_'
@@ -24,6 +28,7 @@ def selector(case, **kwargs):
 other_names = ['ett30', 'ett30I01']
 other_dir = ['./excel/ett30.xlsx', './excel/ett30I01.xlsx']
 selector(case=1, loader_excel='./excel/Data_loader_Round13.xlsx', other_names=other_names, other_dir=other_dir,
-         write_dir_name='Round13_invariant', k_folds=3, model_mode='ann', scoring='mse', total_run=15, random_run=1)
+         write_dir_name='Round13_invariant', k_folds=3, model_mode='ann', scoring='mse', total_run=15, random_run=1,
+         data_augmentation='invaiant', numel=3)
 
 
