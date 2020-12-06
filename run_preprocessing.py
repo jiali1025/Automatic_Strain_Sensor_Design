87b5ad5dd1676526c4911790c58689a5ea29f155
Lim Kai Zhuo <limkaizhuo@gmail.com>
2020-12-06 21:22:11 +0800

    Added code for L2 distance calculation

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -1,4 +1,4 @@
-from own_package.preprocessing import read_excel_data_to_cutoff, read_grid_data
+from own_package.preprocessing import read_excel_data_to_cutoff, read_grid_data, l2_tracker
 from own_package.others import create_results_directory
 
 
@@ -14,6 +14,11 @@ def run_preprocess(select):
         write_dir = create_results_directory('./results/preprocessing/grid')
         read_grid_data(read_excel_file='./excel/grid.xlsx', write_dir=write_dir)
 
+    elif select == 3:
+        l2_tracker(write_excel_dir='./results/preprocessing/l2_information.xlsx',
+                   final_excel_loader='./excel/Data_loader_Round13.xlsx',
+                   last_idx_store=[11, 16, 21, 29, 37, 45, 69, 77, 85, 93, 101, 109, 117, 125])
+
 
 if __name__ == '__main__':
-    run_preprocess(1)
+    run_preprocess(3)
