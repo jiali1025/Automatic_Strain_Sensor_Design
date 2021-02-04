9e49de3901dc22200cfdacc93eb55bbb9fd67235
jiali1025 <lijiali1994@gmail.com>
2021-02-04 20:07:23 +0800

    Add files via upload

--- run_demo_svm_training.py
+++ run_demo_svm_training.py
@@ -9,7 +9,7 @@ def selector(case):
         svm_hparam_opt(grid_fl_dir=grid_fl_dir, total_run=20, write_excel_dir='./results/svm_hparam_opt.xlsx')
 
     elif case == 2:  # Run svm_classifier for a particular value of gamma and save those models
-        grid_fl_dir = './results/preprocessing/grid/grid_data'
+        grid_fl_dir = './demo/grid/grid_data'
         results_dir = create_results_directory('./results/svm_classifier/gamma130', folders=['models'])
         run_classification(grid_fl_dir=grid_fl_dir, write_dir=results_dir, gamma=130)
 
